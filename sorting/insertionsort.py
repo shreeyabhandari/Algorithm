f3a459362d31b83a770e8db5210725b4fd37ba07
Shreeya Bhandari <shreeyabhandari66@gmail.com>
2020-03-30 00:39:29 +0200

    update insertionsort

--- sorting/insertionsort.py
+++ sorting/insertionsort.py
@@ -1,1 +1,9 @@
 def insertion_sort(tab):
+  n = len(tab)
+  for i in range(l, n):
+    j = i
+    while (j > 0) and (tab[j] < tab[j - 1]):
+      tab[j], tab[j-1] =tab[j - 1], tab[j]
+      j -= 1
+  return tab
+ 
