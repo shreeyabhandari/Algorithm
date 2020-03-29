eb5141bb634298f77d040c938c4e04d9bdf51f54
Shreeya Bhandari <shreeyabhandari66@gmail.com>
2020-03-30 01:14:32 +0200

    update queuealgo

--- Datastructure/queuealgo.py
+++ Datastructure/queuealgo.py
@@ -1,1 +1,35 @@
+def create_queue(): # create a queue
+  return []
+
+
+
+def is_empty(q): #check whether queue is empty
+  return q == []
+
+
+
+
+def peek(q):  # returns the first item of the queue
+  if is_empty(q):
+    print('Error, the queue is empty')
+  else:
+    return q[0]
+  
+  
+  
+def enqueue(q, x): # add an element to the queue
+  q.append(x)
+  return q
+
+
+def de_queue(q):  #remove an element of the queue
+  q.pop(0)
+  return q
+
+
+  
+  
+  
+ 
+
 
