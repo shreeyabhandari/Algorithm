53f04d9a90c87dca691e04c68c9cdaecadf4b2a9
Shreeya Bhandari <shreeyabhandari66@gmail.com>
2020-03-30 01:21:16 +0200

    update stackalgo

--- Datastructure/stackalgo.py
+++ Datastructure/stackalgo.py
@@ -1,1 +1,27 @@
 
+def create_stack():   # create a stack
+  return []
+
+
+def is_empty(q):   #check whether the stack is empty 
+  return q == []
+
+
+def peek(q):   # return the first item of the stack
+  if is_empty(q):
+    print('Error, the stack is empty')
+  else:
+    return q[0]
+  
+  
+def s_push(s, x):   # add an element to the stack
+  if is_empty(s):
+    s.append(x)
+  else:
+    s = [x]+s
+  return s
+
+
+def s_stack(s):   # remove an element of the stack
+  s.pop(0)
+  return s
