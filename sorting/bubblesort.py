42848d93999b7221d44e55bd3904cb09821c7c2f
Shreeya Bhandari <shreeyabhandari66@gmail.com>
2020-03-30 00:49:01 +0200

    update bubble sort

--- sorting/bubblesort.py
+++ sorting/bubblesort.py
@@ -1,1 +1,13 @@
-
+def bubble_sort(tab):
+  swap = true
+  n = len(tab)
+  while swap:
+    swap = false
+    for i in range(n-1):
+      if tab[i] > tab[i + 1]:
+        tab[i], tab[i + 1] = tab[i + 1], tab[i]
+        swap = true
+    n = n - 1
+  return tab
+        
+  
