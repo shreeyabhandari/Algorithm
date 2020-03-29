4e88482201cf779046f27bbcd96221dad796c707
Shreeya Bhandari <shreeyabhandari66@gmail.com>
2020-03-30 00:44:40 +0200

    update selection sort file

--- sorting/selectionsort.py
+++ sorting/selectionsort.py
@@ -1,1 +1,10 @@
-def selection_sort(tab)
+def selection_sort(tab):
+  n = len(tab)
+  for i in range(n):
+    min_pos = i
+    for j in range(i + 1, n):
+      if tab[j] < tab[min_pos]:
+        min_pos = j
+    tab[i], tab[min_pos] = tab[min_pos], tab[i]
+  return tab  
+
